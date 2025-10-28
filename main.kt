a4b479853c6f151d9915bbc1a4a0204e4ad8a59e
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-10-28 19:35:24 -0300

    Add main.kt with initial Kotlin program

---
+++ main.kt
@@ -0,0 +1,20 @@
+package net_kotlin_app
+// -------------INFORMAÇÕES QUE VI NO CURSO---------------  //
+// Val é uma variável imutável                              //
+// Var é uma variável mutável                               //
+// Lembrando ~nossos programas são burros~                  //
+// Val(s), Var(s) são declarações de espaço de memória      //
+// ----------------METAS----------------------------------- //
+// [ ] Aprender Kotlin                                      //
+// [ ] Terminar o Curso                                     //
+// [ ] Aprender Kord                                        //
+//--------------------------------------------------------- //
+fun main() {
+    val price = 31.90
+    val product = "iMac"
+    val age = 31
+
+    println(price)
+    println(product)
+    println(age)
+}
